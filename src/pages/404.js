f25448bface2128769b2b23d8780c785a128810d
zcoursey22 <zcoursey22@gmail.com>
2019-03-09 13:06:07 -0500

    Adds custom 404 page for mobile

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,14 +1,61 @@
 import React from "react"
-
+import Button from "@material-ui/core/Button"
 import Layout from "../components/layout"
+import { Link } from "gatsby"
 import SEO from "../components/seo"
+import styled from "styled-components"
+import { styles } from "../utils"
 
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <NotFoundPageWrapper>
+      <div id="404-main">
+        <h1>Oops!</h1>
+        <h2>Error code 404</h2>
+        <p>The page you're trying to access doesn't exist.</p>
+      </div>
+      <Link to="/">
+        <Button variant="contained">Go Home</Button>
+      </Link>
+    </NotFoundPageWrapper>
   </Layout>
 )
+
+const NotFoundPageWrapper = styled.div`
+  padding: 0 1.5rem;
+  max-width: 45rem;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  h1 {
+    font-family: "Oswald";
+    font-size: 7rem;
+    /* line-height: 1.25; */
+  }
+  h2 {
+    font-family: "Roboto";
+    color: ${styles.colors.grey};
+    text-transform: uppercase;
+    letter-spacing: 1px;
+    font-weight: 700;
+    font-size: 1rem;
+  }
+  p {
+    font-family: "Noto Sans";
+    padding: 1rem 0;
+  }
+  button {
+    color: ${styles.colors.white};
+    background: ${styles.colors.primary};
+    &:hover {
+      background: ${styles.colors.primaryDark};
+    }
+  }
+  a {
+    text-decoration: none;
+  }
+`
 
 export default NotFoundPage
--- src/utils/styles.js
+++ src/utils/styles.js
@@ -1,6 +1,8 @@
 export const colors = {
   primary: "#3a7",
+  primaryDark: "#296",
   secondary: "#f55",
+  secondaryDark: "#e44",
   black: "#111",
   darkGrey: "#666",
   grey: "#ccc",
