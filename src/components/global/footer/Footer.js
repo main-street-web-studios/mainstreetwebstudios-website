b2cea77a610cc96bbe489eae1dafa340143c6410
zcoursey22 <zcoursey22@gmail.com>
2019-03-10 00:44:08 -0500

    Adds nav links in footer

--- src/components/global/footer/Footer.js
+++ src/components/global/footer/Footer.js
@@ -1,12 +1,54 @@
 import React, { Component } from "react"
 import styled from "styled-components"
 import { styles } from "../../../utils"
+import { Button } from "@material-ui/core"
+import { Link } from "gatsby"
 
 export default class Footer extends Component {
+  state = {
+    links: [
+      {
+        id: 0,
+        path: "/what-we-do",
+        name: "what we do",
+      },
+      {
+        id: 1,
+        path: "/story",
+        name: "our story",
+      },
+      {
+        id: 2,
+        path: "/contact",
+        name: "contact us",
+      },
+    ],
+  }
+
   render() {
+    const { links } = this.state
     return (
       <FooterWrapper>
-        <p id="legal">
+        <div id="main">
+          <ul className="nav">
+            {links.map(link => (
+              <li className="nav">
+                <Link className="nav-link" to={link.path} key={link.id}>
+                  {link.name}
+                </Link>
+              </li>
+            ))}
+          </ul>
+          {/*<div>
+            <div id="input">
+              <input type="text" defaultValue="" placeholder="Email Address" />
+            </div>
+            <Button variant="contained" className="btn">
+              Subscribe
+            </Button>
+          </div>*/}
+        </div>
+        <p id="copyright">
           &copy;{new Date().getFullYear()} Main Street Web Studios
         </p>
       </FooterWrapper>
@@ -23,8 +65,55 @@ const FooterWrapper = styled.div`
   font-family: "Noto Sans";
   padding: 1rem 1.5rem;
   border-top: 0.25rem solid ${styles.colors.primary};
-  #legal {
+  #main {
+    display: flex;
+    align-items: space-between;
+    > * {
+      flex-grow: 1;
+    }
+  }
+  #input {
+    background: ${styles.colors.white};
+    border-radius: 3px;
+    margin: 0.5rem 0;
+    > input {
+      display: block;
+      width: 100%;
+      border: none;
+      background: none;
+      padding: 0.8rem 0.5rem;
+      font-size: 1rem;
+    }
+  }
+  ul.nav {
+    text-align: right;
+  }
+  li.nav {
+    font-size: 1rem;
+    list-style: none;
+    margin-top: 0.5rem;
+    &:nth-of-type(1) {
+      margin-top: 0;
+    }
+  }
+  .nav-link {
+    color: ${styles.colors.lightGrey};
+    text-decoration: none;
+    text-transform: uppercase;
+    font-weight: 700;
+  }
+  .btn {
+    font-weight: 700;
+    font-size: 1rem;
+    width: 100%;
+    padding: 0.5rem 2rem;
+    background: ${styles.colors.primary};
+    color: ${styles.colors.white};
+    margin: 0.5rem 0;
+  }
+  #copyright {
     text-align: center;
     font-size: 0.9rem;
+    margin-top: 1.5rem;
   }
 `
