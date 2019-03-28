6094e091f9ef34f7086fea5236a98cf4b147102a
zcoursey22 <zcoursey22@gmail.com>
2019-03-27 20:54:08 -0400

    Adds desktop view for about page

--- src/components/about/MeetOurTeam.js
+++ src/components/about/MeetOurTeam.js
@@ -31,6 +31,9 @@ export default () => {
       font-weight: 700;
       font-family: "Noto Sans";
     }
+    .team-members-container {
+      display: block;
+    }
     .team-member {
       display: flex;
       flex-direction: column;
@@ -54,6 +57,23 @@ export default () => {
         height: 50vw;
       }
     }
+    @media (min-width: 768px) {
+      .team-members-container {
+        display: flex;
+        align-items: center;
+        justify-content: space-evenly;
+      }
+      .team-member-avatar {
+        > * {
+          max-width: 22rem;
+          min-width: 15rem;
+          width: 40vw;
+          max-height: 22rem;
+          min-height: 15rem;
+          height: 40vw;
+        }
+      }
+    }
   `
 
   return (
@@ -65,19 +85,21 @@ export default () => {
           and technology. Whether its design, marketing, or development, our
           team works for you.
         </p>
-        <div className="team-member">
-          <Paper className="team-member-avatar">
-            <Avatar src="https://scontent-mia3-1.xx.fbcdn.net/v/t1.0-9/49555784_1404509823013191_3202670500316708864_n.jpg?_nc_cat=105&_nc_ht=scontent-mia3-1.xx&oh=6130a04ec965514f491dc40dbce79163&oe=5D0C0177" />
-          </Paper>
-          <h3>Zach Coursey</h3>
-          <h4>WEB DEVELOPER</h4>
-        </div>
-        <div className="team-member">
-          <Paper className="team-member-avatar">
-            <Avatar src="https://images.unsplash.com/photo-1527887980323-88b77f646535?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80" />
-          </Paper>
-          <h3>Kent Carson</h3>
-          <h4>UI/UX DESIGNER</h4>
+        <div className="team-members-container">
+          <div className="team-member">
+            <Paper className="team-member-avatar">
+              <Avatar src="https://scontent-mia3-1.xx.fbcdn.net/v/t1.0-9/49555784_1404509823013191_3202670500316708864_n.jpg?_nc_cat=105&_nc_ht=scontent-mia3-1.xx&oh=6130a04ec965514f491dc40dbce79163&oe=5D0C0177" />
+            </Paper>
+            <h3>Zach Coursey</h3>
+            <h4>WEB DEVELOPER</h4>
+          </div>
+          <div className="team-member">
+            <Paper className="team-member-avatar">
+              <Avatar src="https://images.unsplash.com/photo-1527887980323-88b77f646535?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80" />
+            </Paper>
+            <h3>Kent Carson</h3>
+            <h4>UI/UX DESIGNER</h4>
+          </div>
         </div>
       </div>
     </MeetOurTeam>
